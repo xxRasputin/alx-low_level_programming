f29135d93725f99e1ee3c9de970764aa6245e9c0
xxRasputin <ushernjuguna@gmail.com>
2022-09-10 10:57:09 -0700

    first real C code

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+/**
+ *
+ * main - entry point
+ *
+ * return: 0 (done succesfully)
+ *
+ */
+
+int main(void)
+{
+  
+	puts("Programming is like building a multilingual puzzle/n");
+	return(0);
+    
+}
