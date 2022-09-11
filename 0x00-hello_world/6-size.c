c12e7778645e134d61227bac8bcd53d8839f6de3
xxRasputin <ushernjuguna@gmail.com>
2022-09-11 08:16:54 -0700

    spacing

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,10 +13,10 @@ int main(void)
 	long long int lli;
 	float f;
 
-	printf("size of a char: %lu byte(s)\n", (unsigned long)size of(c));
-	printf("size of an int: %lu byte(s)\n", (unsigned long)size of(i));
-	printf("size of a long int: %lu byte(s)\n", (unsigned long)size of(li));
-	printf("size of a long long int: %lu byte(s)\n", (unsigned long)size of(lli));
-	printf("size of a float: %lu byte(s)\n", (unsigned long)size of(f));
+	printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
+	printf("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(li));
+	printf("size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(lli));
+	printf("size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 	return (0);
 }
