7b905dfa4d676b0af1ad6e151cf2a55d15fc50ef
xxRasputin <ushernjuguna@gmail.com>
2022-09-10 19:09:00 -0700

    getSize

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+	int i;
+	long int li;
+	long long int lli;
+	float f;
+
+	printf("size of a char: %lu byte(s)\n", (unsigned long)size of(c));
+	printf("size of an int: %lu byte(s)\n", (unsigned long)size of(i));
+	printf("size of a long int: %lu byte(s)\n", (unsigned long)size of(li));
+	printf("size of a long long int: %lu byte(s)\n", (unsigned long)size of(lli));
+	printf("size of a float: %lu byte(s)\n", (unsigned long)size of(f));
+	return (0);
+}
