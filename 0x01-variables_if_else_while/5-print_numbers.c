f8a9ad6837c5f487c227b09d476efe153e2ab243
xxRasputin <ushernjuguna@gmail.com>
2022-09-12 01:55:48 -0700

    specify variable

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Accept 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	n = 0;
+	while (n < 10)
+	{
+		printf("%d", n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
