fc58e960864a931bb221e5a69fbab80c9e441d7b
xxRasputin <ushernjuguna@gmail.com>
2022-09-14 17:05:06 -0700

    header issue

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char putch[10] = "_putchar";
+
+	int n = 0;
+
+	while (i < 8)
+	{
+		_putchar(putch[i]);
+		i++;
+	}
+	_putchar(10);
+	return (0);
+}
