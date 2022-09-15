912ff974e66c1dc062ff9968c126abf195ad97a8
xxRasputin <ushernjuguna@gmail.com>
2022-09-15 14:25:25 -0700

    peezy

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - add 2 integers
+ *
+ * @a: first input
+ * @b: second input
+ *
+ * Return: sum of a and b
+ */
+int add(int a, int b)
+{
+	int sum = a + b;
+
+	return (sum);
+}
