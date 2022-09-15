f3cd1cedbc1d1df6b0fecc4a70f91dabb6be203d
xxRasputin <ushernjuguna@gmail.com>
2022-09-15 11:11:07 -0700

    git commit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_last_digit - computes absolute values
+ *
+ * @n: parameter to be checked
+ *
+ * Return: last digit
+ */
+int print_last_digit(int n)
+{
+	int d = n % 10;
+
+	if (d < 0)
+		d = -n;
+
+	_putchar(d + '0');
+	return (d);
+}
