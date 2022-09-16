ff0d1b6e800289b4e7accbe4d0c8c43ca4d82580
xxRasputin <ushernjuguna@gmail.com>
2022-09-16 13:03:42 -0700

    betty issues

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+* main - causes an infinite loop
+* Return: 0
+*/
+int main(void)
+{
+	int i;
+
+	printf("infinit loop incoming :(\n");
+
+	i = 0;
+
+	while (i < 10)
+	{
+		putchar(i);
+	}
+
+	printf("infinit loop avoided! \\o/\n");
+
+	return (0);
+}
