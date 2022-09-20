284d090ab0dae7b0895c2199113214d101e7296f
xxRasputin <ushernjuguna@gmail.com>
2022-09-20 03:01:09 -0700

    gcc error

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse string
+ * @s: string
+ * Return: void
+ *
+ */
+void rev_string(char *s)
+{
+	int i, max, half;
+
+	char first, last;
+	{
+		i++;
+	}
+	max = i - 1;
+	half = max / 2;
+	while (half >= 0)
+	{
+		first = s[max - half];
+		last = s[half];
+		s[half] = first;
+		s[max - half] = last;
+		half--;
+	}
+}
