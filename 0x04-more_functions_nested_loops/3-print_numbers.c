c0e8aaa7a481b0852d982de53799a1f5db076cb5
xxRasputin <ushernjuguna@gmail.com>
2022-09-21 12:35:00 -0700

    betty linter

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints 0-9 using _putchar
+ *
+ * Return: Always 0
+ *
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		_putchar(i + '0');
+	_putchar(10);
+}
