974a52144e38f1f1f60e76b1f25f081eb651f203
xxRasputin <ushernjuguna@gmail.com>
2022-10-02 14:55:55 -0700

    gcc compilation errors fix

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - function that prints its name
@@ -9,19 +10,30 @@
  *
  * Return: 0 for success 1 otherwise
  */
+
 int main(int argc, char *argv[])
 {
-	int result, num1, num2;
+	int result = 0, num, i, j, k;
 
-	if (argc != 3)
+	if (argc == 1)
+		printf("0\n");
+	for (i = 1; i < argc; i++)
 	{
-		printf("%s\n", "Error");
-		return (1);
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] > '9' || argv[i][j] < '0')
+			{
+				printf("%s\n", "Error");
+				return (1);
+			}
+		}
 	}
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[2]);
-	result = num1 * num2;
 
+	for (k = 1; k < argc; k++)
+	{
+		num = atoi(argv[k]);
+		result += num;
+	}
 	printf("%d\n", result);
 	return (0);
 }
