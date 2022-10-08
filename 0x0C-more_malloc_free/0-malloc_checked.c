97d2610810d74a79e606d7b146ceab9721357d54
xxRasputin <111707837+xxRasputin@users.noreply.github.com>
2022-10-08 03:22:13 -0500

    hot fix

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * malloc_checked - allocates memory using malloc
+ *
+ * @b: unsigned int of amount of needed memory
+ *
+ * Return: void
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *p;
+
+	p = malloc(b);
+	if (p == NULL)
+		exit(98);
+	return (p);
+}
