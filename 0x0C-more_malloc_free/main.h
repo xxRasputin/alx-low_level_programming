17f32ebae9efcca11dba0e7cb94783193d505840
xxRasputin <ushernjuguna@gmail.com>
2022-10-08 01:37:43 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,6 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
 void *_calloc(unsigned int nmemb, unsigned int size);
